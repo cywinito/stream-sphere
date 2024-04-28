20b0ceae8a8fb4922f050c9577e340f7b09aa721
cywinito <alexcywiner@hotmail.com>
2024-04-28 17:25:37 -0300

    Update metadata description

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "StreamSphere",
-  description: "Streaming platform for programmers",
+  description: "Entertainment streaming platform",
 };
 
 export default function RootLayout({
